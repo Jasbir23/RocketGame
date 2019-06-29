0f114bb37b8e26060875b29072ff4287fd542ac5
Jasbir23 <jasbirshergill23@gmail.com>
2019-06-30 02:19:12 +0530

    Updates

---
+++ precache-manifest.f2d8bca8e38a21d599355c8d567b810b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6208310a586b5049d930347e381d1996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e0d88d69958ce53396",
+    "url": "/static/css/main.3e412261.chunk.css"
+  },
+  {
+    "revision": "8aaddaefaf69101ce4a8",
+    "url": "/static/js/2.df410ec5.chunk.js"
+  },
+  {
+    "revision": "18e0d88d69958ce53396",
+    "url": "/static/js/main.dc00c460.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
